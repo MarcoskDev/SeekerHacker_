ec7872b498f389e1828eb454a6db41634cb46c30
ShadeHacker <154840423+MarcoskDev@users.noreply.github.com>
2025-06-21 18:42:53 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+// Botão de ajuda (futuro chat ou redirecionamento)
+document.getElementById('ajuda-btn').addEventListener('click', function () {
+  alert('Você será redirecionado para o atendimento.');
+  // window.location.href = "https://link-do-suporte.com"; // implementar depois
+});
